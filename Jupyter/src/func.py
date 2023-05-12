45a407eec10bb80e170efddfd87c0338bc1a6b21
gusavato <augusto.abad@outloock.com>
2023-05-12 10:55:42 +0200

    primer commit

---
+++ Jupyter/src/func.py
@@ -0,0 +1,13 @@
+"""
+Archivo de funciones usadas en el proyecto de visualización de la bootcamp
+de Data Analytics de IRONHACK de Abril - 2023
+"""
+
+
+def format_date(date : str) -> dict:
+"""
+Función que recibe una string de una fecha en formato "%dsemana %dmes %mes 
+%año" y devuelve un diccionario con la siguiente información: día de la semana,
+día mes, mes, año, weekend}
+"""
+
